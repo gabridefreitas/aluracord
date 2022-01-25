f6b9425178271296b523842b11f3e37a68af5923
Gabriel de Freitas <contato.gabrieldefreitas@gmail.com>
2022-01-24 21:39:36 -0300

    feat: :tada: first commit

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+function GlobalStyle() {
+  return (
+    <style jsx>{`
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+      }
+      body {
+        font: sans-serif;
+      }
+    `}</style>
+  );
+}
+
+function HomePage() {
+  return (
+    <div>
+      <GlobalStyle />
+      Welcome to Nextjs!
+      <style jsx>{`
+        div {
+          background-color: red;
+        }
+      `}</style>
+    </div>
+  );
+}
+
+export default HomePage;
